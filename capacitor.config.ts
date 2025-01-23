322206e081672a149c92597904b1c4d5f720f200
Ken Sodemann <ken.sodemann@gmail.com>
2025-01-23 14:20:00 -0600

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.testpush',
+  appName: 'test-pns',
+  webDir: 'www/browser',
+};
+
+export default config;
